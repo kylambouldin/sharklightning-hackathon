71eedcfc8358b73b97e6cc1d0ecbef48feab7f15
john <john@john.(none)>
2014-04-11 23:36:50 -0400

    added view for big board

--- simpler/ermanager/views.py
+++ simpler/ermanager/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from ermanager.models import Paitent, Location
+from django.http import HttpResponse
 # Create your views here.
+
+def big_board(request):
+	rooms = Location.objects.order_by('name')
+	fnames = Paitent.objects.first_name
+	lnames = Paitent.objects.last_name
+	wait = Paitent.objects.isWaiting
+	return HttpResponse(rooms)
+	
