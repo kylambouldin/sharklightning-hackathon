012c8c31f08b65f1d4a27c0318bd54a8464713e1
notori0us <cbwcjw@gmail.com>
2014-04-12 03:32:25 -0400

    added landing page controller

---
+++ simpler/simpler/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'simpler/home.html')
