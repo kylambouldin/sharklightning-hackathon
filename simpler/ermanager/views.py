47c4fe7c8ac2100ba5978f9cfdf857686201023d
kvaratskhelia <kvaratskhelia9@gmail.com>
2014-04-12 05:20:09 -0400

    FORMS! I have them working?

--- simpler/ermanager/views.py
+++ simpler/ermanager/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from ermanager.models import Patient, Location
+from django import forms
+import datetime
 
 from django.http import HttpResponse
 from django.template import RequestContext, loader
@@ -14,7 +16,7 @@ def doctor_page(request):
     })
   return HttpResponse(template.render(context))
 
-def patient_edit(request,paitent_id):
+def patient_edit(request,patient_id):
   try:
     patient = Patient.objects.get(id=patient_id)
   except Patient.DoesNotExist:
@@ -23,12 +25,38 @@ def patient_edit(request,paitent_id):
 
 # Create your views here.
 
+def submit_edit(request):
+  if request.method == 'POST':
+    form = PatientForm(request.POST)
+    if form.is_valid():
+      patient = Patient()
+      patient.last_name = form.cleaned_data['last_name']
+      patient.first_name = form.cleaned_data['first_name']
+      patient.needs_review = form.cleaned_data['needs_review']
+      patient.is_waiting = False
+      loc_temp = Location(number=12)
+      loc_temp.save()
+      patient.loc = loc_temp
+      patient.last_moved = "2010-10-11 10:56"
+      patient.last_checkup = "2010-10-11 10:56"
+      patient.priority = "LOW"
+      patient.nurse_notes = "Notes"
+      patient.doctor_notes = "Dummy notes"
+      patient.save()
+  patient_list = Patient.objects.order_by('-last_name')
+  template= loader.get_template('ermanager/doctor_view.html')
+  context = RequestContext(request, {
+      'patient_list':patient_list,
+    })
+  return HttpResponse(template.render(context))
+
 def patient_mod(request,patient_id):
   try:
     patient = Patient.objects.get(id=patient_id)
+    form = PatientForm(initial={'last_name':patient.last_name})
   except Patient.DoesNotExist:
     raise Http404
-  return render(request, 'ermanager/patient_mod.html', {'patient':patient})
+  return render(request, 'ermanager/patient_mod.html', {'patient':patient,'form':form})
 
 def big_board(request):
     rooms = Location.objects.order_by('number')
@@ -45,3 +73,24 @@ def big_board(request):
           'dict1':dict1,
     })
     return HttpResponse(template.render(context))
+
+
+
+#Form definition
+
+class PatientForm(forms.Form):
+  last_name = forms.CharField()
+  first_name = forms.CharField()
+  needs_review = forms.BooleanField()
+#  is_waiting = forms.BooleanField()
+#  last_moved = forms.DateTimeField("last moved")
+#  last_checkup = forms.DateTimeField("last checkup")
+#  PRIORITY_CHOICES = (
+#          ('CR', 'Critical'),
+#          ('HI', 'High'),
+#          ('MED', 'Medium'),
+#          ('LOW', 'Low'),
+#  )
+#  priority = forms.CharField(max_length=3, choices=PRIORITY_CHOICES)
+#  nurse_notes = forms.TextField()
+#  doctor_notes = forms.TextField()
