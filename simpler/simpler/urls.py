ed9d0c7c75c77fe5fdbedeee385fc0217d6d136f
notori0us <cbwcjw@gmail.com>
2014-04-12 03:30:45 -0400

    added a landing page.

--- simpler/simpler/urls.py
+++ simpler/simpler/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
+import simpler.views
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('', 
     # Examples:
     # url(r'^$', 'simpler.views.home', name='home'),
     # url(r'^simpler/', include('simpler.urls')),
+    url(r'^$', simpler.views.home),
 
     url(r'^ermanager/', include('ermanager.urls')),
 
