49836e6150379507c856fdac841db86fec9e1382
kvaratskhelia <kvaratskhelia9@gmail.com>
2014-04-12 08:31:32 -0400

    Forms with full functionality

--- simpler/ermanager/views.py
+++ simpler/ermanager/views.py
@@ -28,20 +28,24 @@ def patient_edit(request,patient_id):
 def submit_edit(request):
   if request.method == 'POST':
     form = PatientForm(request.POST)
+    print form.errors
+    pat_id = request.session.get('patient_id')
+    print str(pat_id)+"LETS SEE"
     if form.is_valid():
-      patient = Patient()
+      patient = Patient.objects.get(id=pat_id)
       patient.last_name = form.cleaned_data['last_name']
       patient.first_name = form.cleaned_data['first_name']
       patient.needs_review = form.cleaned_data['needs_review']
-      patient.is_waiting = False
-      loc_temp = Location(number=12)
+      patient.is_waiting = form.cleaned_data['is_waiting']
+      var = form.cleaned_data['loc']
+      loc_temp = Location.objects.get(id=var)
       loc_temp.save()
       patient.loc = loc_temp
-      patient.last_moved = "2010-10-11 10:56"
-      patient.last_checkup = "2010-10-11 10:56"
-      patient.priority = "LOW"
-      patient.nurse_notes = "Notes"
-      patient.doctor_notes = "Dummy notes"
+      patient.last_moved = form.cleaned_data['last_moved']
+      patient.last_checkup = form.cleaned_data['last_checkup']
+      patient.priority = form.cleaned_data['priority']
+      patient.nurse_notes = form.cleaned_data['nurse_notes']
+      patient.doctor_notes = form.cleaned_data['doctor_notes']
       patient.save()
   patient_list = Patient.objects.order_by('-last_name')
   template= loader.get_template('ermanager/doctor_view.html')
@@ -53,9 +57,10 @@ def submit_edit(request):
 def patient_mod(request,patient_id):
   try:
     patient = Patient.objects.get(id=patient_id)
-    form = PatientForm(initial={'last_name':patient.last_name})
+    form = PatientForm(initial={'last_name':patient.last_name,'first_name':patient.first_name,'needs_review':patient.needs_review,'is_waiting':patient.is_waiting ,'loc':patient.loc, 'last_moved':patient.last_moved ,'last_checkup': patient.last_checkup ,'priority': patient.priority ,'nurse_notes': patient.nurse_notes , 'doctor_notes':patient.doctor_notes})
   except Patient.DoesNotExist:
     raise Http404
+  request.session['patient_id'] = patient_id
   return render(request, 'ermanager/patient_mod.html', {'patient':patient,'form':form})
 
 def big_board(request):
@@ -81,16 +86,11 @@ def big_board(request):
 class PatientForm(forms.Form):
   last_name = forms.CharField()
   first_name = forms.CharField()
-  needs_review = forms.BooleanField()
-#  is_waiting = forms.BooleanField()
-#  last_moved = forms.DateTimeField("last moved")
-#  last_checkup = forms.DateTimeField("last checkup")
-#  PRIORITY_CHOICES = (
-#          ('CR', 'Critical'),
-#          ('HI', 'High'),
-#          ('MED', 'Medium'),
-#          ('LOW', 'Low'),
-#  )
-#  priority = forms.CharField(max_length=3, choices=PRIORITY_CHOICES)
-#  nurse_notes = forms.TextField()
-#  doctor_notes = forms.TextField()
+  needs_review = forms.BooleanField(required=False)
+  is_waiting = forms.BooleanField(required=False)
+  loc = forms.CharField()
+  last_moved = forms.DateTimeField()
+  last_checkup = forms.DateTimeField()
+  priority = forms.CharField(max_length=3)
+  nurse_notes = forms.CharField()
+  doctor_notes = forms.CharField()
