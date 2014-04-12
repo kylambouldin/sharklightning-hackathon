08ab043be6e324211c11c5f0dd3329bb87ea2dc2
notori0us <cbwcjw@gmail.com>
2014-04-11 21:06:06 -0400

    enabled the admin site for our ermanager models

--- simpler/ermanager/admin.py
+++ simpler/ermanager/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from ermanager.models import Location, LastMoved, LastCheckup
 
 # Register your models here.
+# register location
+admin.site.register(Location)
+
+admin.site.register(LastMoved)
+
+admin.site.register(LastCheckup)
